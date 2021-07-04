29b6c1e9ebe19157a2e53f5a0467f5aa49d2234c
Artem Bykov <bykovaa94@mail.ru>
2021-07-04 18:17:15 +0300

    task 2

--- bin/commander.js
+++ bin/commander.js
@@ -4,9 +4,12 @@ import { Command } from 'commander/esm.mjs';
 const commander = new Command();
 
 commander
+  .name('gendiff')
+  .usage('[options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .option('-V, --version', 'output the version number');
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format');
 
 commander.parse(process.argv);
 const options = commander.opts();
